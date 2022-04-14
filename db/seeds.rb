7c930c95db5215c04977e4946db2f384dd6f2731
bhargav <sbbhargav1993@gmail.com>
2022-04-14 14:20:31 +0530

    implemted show for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ puts "5 skills  r created"
 	Portfolio.create!(
 		title: "Portfolio title #{port_item}" ,
 		body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ,
-		main_image: "https://via.placeholder.com 600x400",
+		main_image: "https://via.placeholder.com/600x400",
 		tumb_image: "https://via.placeholder.com/300x150"
 	)
 end
