ee2398c1a022aff031ede70eaff4214aa3b33a00
bhargav <sbbhargav1993@gmail.com>
2022-04-24 15:23:53 +0530

    implemtented content tag helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
 			(link_to "Register", new_user_registration_path )
     end 
 	end
+
+	def source_helper(layout_name)
+		if session[:source]
+			greeting="Thanks for visiting me from #{session[:source]} #{layout_name}"	
+    	content_tag(:p,greeting, class: "source-greeting")
+    end
+	end
 end
